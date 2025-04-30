5d2f6f2eeb0867e1f3e0252b078f81c8d29dcce1
abdulaziz <abdulazizakbarov065@gmail.com>
2025-04-30 11:00:28 +0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,21 @@ import useMyStor from "./Main/Store/Mystore";
 import LoginPage from "./Main/Login";
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, _] = useState(false);
   const state = useMyStor();
   return (
-    <div>
+    <div className="h-full">
       {state.accessToken ? (
         <div className="h-full">
-          <nav className="flex justify-between  py-2 px-6 bg-[#324861]">
-            <h2 className="text-3xl font-bold pt-1">Logo</h2>
+          <nav className="flex justify-between  py-2 px-6 bg-[#001529]">
+            <h2 className="text-3xl font-bold pt-1 text-white">Logo</h2>
             <div>
-              <h2 className="text-xl font-semibold">Abdulaziz</h2>
-              <h2 className="text-xl font-semibold">Akbarov</h2>
+              <h2 className="text-xl font-semibold text-white">Abdulaziz</h2>
+              <h2 className="text-xl font-semibold text-white">Akbarov</h2>
             </div>
           </nav>
-          <div className="flex justify-start h-full w-[1500px]">
-            <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+          <div className="flex  justify-start screen w-[1500px]">
+            <Sidebar collapsed={collapsed} />
 
             <MainPage/>
           </div>
--- src/Main/HomePage.tsx
+++ src/Main/HomePage.tsx
@@ -1,10 +1,109 @@
+import { Form, Input, message } from "antd";
+import ReactApexChart from "react-apexcharts";
+import api from "./Axios";
+import { useEffect, useState } from "react";
+
+type Data = {
+  total: string;
+  date: string;
+};
+const ApexChart = ({ data }: { data: Data[] }) => {
+  console.log(data);
+
+  return (
+    <div className="w-[1300px]">
+      {data ? (
+        <ReactApexChart
+          options={{
+            chart: {
+              height: 350,
+              type: "area",
+            },
+            dataLabels: {
+              enabled: false,
+            },
+            stroke: {
+              curve: "smooth",
+            },
+            xaxis: {
+              type: "datetime",
+              categories: data.map((i) => {
+                return i.date;
+              }),
+            },
+            tooltip: {
+              x: {
+                format: "yy/MM/dd",
+              },
+            },
+          }}
+          series={[
+            {
+              name: "datas",
+              data: data.map((i) => {
+                return Number(i.total);
+              }),
+            },
+          ]}
+          type="area"
+          height={350}
+        />
+      ) : (
+        <div>date yoq</div>
+      )}
+    </div>
+  );
+};
 
 function HomePage() {
+  const [data, setData] = useState<Data[]>([]);
+  const [value1, setValue1] = useState<string>("2025-04-10");
+  const [value2, setValue2] = useState<string>("2025-04-29");
+  console.log(value1);
+
+  useEffect(() => {
+    api
+      .post("/api/statistics/daily-order-totals", {
+        startDate: value1,
+        endDate: value2,
+      })
+      .then((res) => {
+        message.success("Date Qo'shildi");
+
+        setData(res.data);
+      })
+      .catch((e) => {
+        message.error(e);
+      });
+  }, [value1, value2]);
+
   return (
     <div>
-      sdfvjkdjsk
+      <ApexChart data={data} />
+
+      <Form
+        layout="vertical"
+        style={{
+          marginLeft: "500px",
+        }}
+      >
+        <Form.Item label={"Boshlanish Sanasi"} className="w-[200px]">
+          <Input
+            type="date"
+            value={value1}
+            onChange={(e) => setValue1(e.target.value)}
+          />
+        </Form.Item>
+        <Form.Item label={"Tugash Sanasi"} className="w-[200px]">
+          <Input
+            type="date"
+            value={value2}
+            onChange={(e) => setValue2(e.target.value)}
+          />
+        </Form.Item>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
